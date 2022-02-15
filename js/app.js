fea7fcb8e8111b907f8151bb07130ec64d8e9ce0
long <hoangphilong2611@gmail.com>
2022-02-15 16:19:38 +0700

    maybe my last commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
+// back to top button
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 200) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#e9b949 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+}
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+// loading screen
+
+setTimeout(function () {
+  $('.loading-wrapper').fadeToggle();
+}, 1500);
